b22a9de147363b416aa7c6abeb51f0f1d3ffd391
git-mathsbox <git@mathsbox.com>
2019-08-16 11:17:40 +0100

    Added provisional post processing method for BARD output.

--- R/bard.R
+++ R/bard.R
@@ -227,3 +227,188 @@ bard<-function(data,p_N,p_A,k_N,k_A,pi_N,alpha,paffected,lower,upper,h)
            )
     
 }
+
+
+
+#### helper functions for post processing
+
+get.probvec <- function( filtering , params , no.draws=1000 )
+{
+  logprobs <- filtering$weights
+  cpt.locs <- filtering$locations
+  types <- filtering$type
+  n <- length(filtering$weights)
+  vec<-numeric(n)
+  prob.states <- matrix(nrow=no.draws,ncol=n)
+  
+  for (i in 1:no.draws){
+  
+    f <- draw.from.post(logprobs, cpt.locs, types, params)
+    segs <- f$draws
+    states <- f$states
+  
+    # fill vec (hist of chpts)
+    vec[f$draws] <- vec[f$draws] + 1
+  
+    # probs of being N or A
+    prob.states[i,] <- get.states(segs,states,n)
+    
+  }
+
+  return( apply(prob.states,2,sum)/no.draws )
+
+}
+
+
+# function to get a vector of states for a drawn value frin filtering posterior # 
+# segs is vector of locations of segments
+# states vector 
+get.states <- function( segs , states, n){
+  
+  state.vec <- numeric(n)
+  state.vec[ segs[1]:segs[2] ] <- states[1]
+  for (i in 2:( length(segs) - 1 ) ){
+    state.vec[ ( segs[i]-1 ):segs[i+1] ] <- states[i]
+  }
+
+  return(state.vec)
+  
+}
+
+format_output = function(R){
+  
+  n = length(R)
+  weights = vector("list", n) 
+  location = vector("list", n)
+  type = vector("list", n)
+  for (i in 1:n){
+    
+    m = length(R[[i]])
+    tmpweights = numeric(2*m)
+    tmplocs = numeric(2*m)
+    tmptypes = numeric(2*m)
+    for (j in 1:m){
+      triple = R[[i]][[j]]
+      tmpweights[(2*j-1):(2*j)] = triple[1:2] 
+      tmplocs[(2*j-1):(2*j)] = as.integer(triple[3])
+      tmptypes[(2*j-1):(2*j)] = c(0,1) 
+    }
+    weights[[i]] = tmpweights[tmpweights > -Inf]
+    location[[i]] = tmplocs[tmpweights > -Inf]
+    type[[i]] = tmptypes[tmpweights > -Inf]
+    
+  }
+  
+  filtering = list("weights"=weights, "locations"=location, "type"=type)
+  return(filtering)
+  
+}
+
+
+# apply loss function to get a segmentation returns vector with 0's and 1's
+# 0 means in a normal segment , 1 is abnormal
+# apply loss with parameter gamma
+
+loss <- function( gamma, probvec ){
+
+  p.gamma <- 1/(1+gamma)
+  probvec[probvec < p.gamma ] <- 0
+  probvec[probvec>0] <- 1
+  return(probvec)
+  
+}
+
+# draw one sample of chpts and states from the posterior
+draw.from.post <- function( logprobs , cpt.locs , types , params ){ 
+
+  k_N = params[1]
+  p_N = params[2]
+  k_A = params[3]
+  p_A = params[4]
+  pi_N = params[5]
+  pi_A = params[6]
+  
+  n <- length(logprobs)
+  l.probs <- logprobs[[n]]
+  seg.locs <- cpt.locs[[n]]
+  seg.types <- types[[n]]
+
+  ind <- 1:length(l.probs)
+  a <- sample( ind , size=1 , replace = TRUE, exp( l.probs ) )
+
+  # state amd location
+  curr.state <- seg.types[a]
+  t <- seg.locs[a]
+
+  STATES <- curr.state
+  DRAW <- t
+  
+  while (t > 1){
+
+    if(curr.state==0){
+    
+      l.probs <- logprobs[[t]][ types[[t]] == 1 ] 
+      i <- cpt.locs[[t]][ types[[t]] ==1 ]
+      back.dens <- log(pi_N) + dnbinom(t-i-1,k_A,p_A,log=TRUE) - pnbinom(t-i-2,k_A,p_A, lower.tail=FALSE , log.p=TRUE)
+      l.probs <- l.probs + back.dens  
+    
+      ind <- 1:length(l.probs)
+      a <- sample( ind , size=1 , replace = TRUE, exp( l.probs ) )
+    
+      # state amd location
+      curr.state <- 1
+      t <- i[a]
+      STATES <- c(STATES,curr.state)
+      DRAW <- c(DRAW,t)
+    }
+    else{
+    
+      # currently in abnormal state poss transitions to normal or abnormal
+      i.N <- cpt.locs[[t]][ types[[t]] == 0 ]
+      i.A <- cpt.locs[[t]][ types[[t]] == 1 ]
+    
+      l.probsA <- logprobs[[t]][  types[[t]] == 1 ] 
+      back.densA <- log(pi_A) + dnbinom(t-i.A-1,k_A,p_A,log=TRUE) - pnbinom(t-i.A-2,k_A,p_A, lower.tail=FALSE , log.p=TRUE)
+      l.probsA <- l.probsA + back.densA
+      # normal points
+      l.probsN <- logprobs[[t]][  types[[t]] == 0 ] 
+      back.densN <- dnbinom( t-i.N-1 , k_N , p_N , log=TRUE ) - pnbinom( t-i.N-2 , k_N , p_N , lower.tail=FALSE , log.p=TRUE )
+      l.probsN <- l.probsN + back.densN
+      # now normalise
+      i.all <- c( i.N , i.A )
+      t.all <- c( rep(0,length(i.N)) , rep(1,length(i.A)) )
+      l.probs <- c( l.probsN , l.probsA )
+      c <- max(l.probs)
+      l.probs.norm <- l.probs - ( c + log( sum( exp( l.probs - c ) ) ) ) 
+    
+      ind <- 1:length(l.probs.norm)
+      a <- sample( ind , size=1 , replace = TRUE, exp( l.probs.norm ) )
+    
+      # state amd location
+      curr.state <- t.all[a]
+      t <- i.all[a]
+      STATES <- c(STATES,curr.state)
+      DRAW <- c(DRAW,t)
+      }
+  
+   }
+  
+  DRAW[DRAW==0] <- 1
+  newlist <- list("draws"=c(n,DRAW),"states"=STATES)
+  return(newlist)
+
+}
+
+# TODO - to be renamed and a S$ class returned with relevant stuff in it
+post_process_bard<-function(bard.result)
+{
+   R<-bard.result@Rs
+   params<-c(bard.result@k_N,bard.result@p_N,bard.result@k_A,bard.result@p_A,bard.result@pi_N,1-bard.result@pi_N)
+   filtering<-format_output(R)
+   # vector of probabilities
+   pvec<-get.probvec(filtering, params, no.draws = 1000)
+   df<-data.frame("time"=seq(1,length(pvec)),"probability"=pvec)
+   p<-ggplot(data=df,aes(x=time,y=probability))
+   p<-p+geom_line()
+   return(p)
+}
