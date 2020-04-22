586910838594502fb250ea78bcf4102a89017cf1
Alex <a.t.fisch@lancaster.ac.uk>
2020-04-22 08:53:41 +0100

    Fixed example.

--- R/capa.R
+++ R/capa.R
@@ -1289,17 +1289,17 @@ capa.uv<-function(x,beta=NULL,beta_tilde=NULL,type="meanvar",min_seg_len=10,max_
 #' 
 #' @examples
 #' library(anomaly)
+#' 
 #' # Simulated data example
-#' set.seed(2018)
 #' # Generate data typically following a normal distribution with mean 0 and variance 1.
 #' # Then introduce 3 anomaly windows and 4 point outliers.
-#' x = rnorm(5000)
-#' x[401:500] = rnorm(100,4,1)
-#' x[1601:1800] = rnorm(200,0,0.01)
-#' x[3201:3500] = rnorm(300,0,10)
+#' 
+#' set.seed(2018)
+#' x  = rnorm(5000)
+#' x[1601:1700] = rnorm(100,0,0.01)
+#' x[3201:3300] = rnorm(100,0,10)
+#' x[4501:4550] = rnorm(50,10,1)
 #' x[c(1000,2000,3000,4000)] = rnorm(4,0,100)
-#' # add some initial data to burnin the sequential estimates
-#' x<-c(rnorm(100,0,1),x)
 #' # use magrittr to pipe the data to the transform
 #' library(magrittr)
 #' trans<-.%>%tierney(1000)
